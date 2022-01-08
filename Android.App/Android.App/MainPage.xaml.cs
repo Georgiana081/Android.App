4edef8109ec4fbce2c8b93c39d24cacf4a950ecc
RAMONA-GEORGIANA CHITICARIU <ramona.chiticariu@stud.ubbcluj.ro>
2022-01-08 18:35:05 +0200

    Add project files.

---
+++ Android.App/Android.App/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Android.App.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Android.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void OnShopListAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = new ShopList()
+            });
+        }
+
+
+    }
+}
---
+++ Android.App/Android.App/Models/ShopList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Android.App.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string Description1 { get; set; }
+        public string Description2 { get; set; }
+        public string Description3 { get; set; }
+        public string Description4 { get; set; }
+        public string Description5 { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
